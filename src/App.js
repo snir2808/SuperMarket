05089597108eb5d6ad15cf350e82df44eaadf009
snir2808 <sniroos@gmail.com>
2020-12-31 13:55:07 +0200

    v3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link} from "react-router-dom";
 import {useHistory} from 'react-router-dom';
 import './App.css';
 import Cart from './component/Cart';
